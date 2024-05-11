e5e22c4a0012191385404de9ad1419c194fc942e
kvongruenigen <kay.vongruenigen@uzh.ch>
2024-05-11 21:24:35 +0200

    Working Trivia

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -11,12 +13,32 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch('questions.json').then(res => {
+fetch('https://opentdb.com/api.php?amount=50&type=multiple')
+  .then(res => {
   console.log(res);
   return res.json();
-}).then(loadedQuestions => {
-  console.log(loadedQuestions);
-  questions = loadedQuestions;
+})
+  .then(loadedQuestions => {
+  console.log(loadedQuestions.results);
+  questions = loadedQuestions.results.map(loadedQuestion => {
+    const formattedQuestion = {
+      question: loadedQuestion.question
+    };
+    
+    const answerChoices = [...loadedQuestion.incorrect_answers];
+    // Randomly select a question
+    formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+    answerChoices.splice(
+      formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+    
+    answerChoices.forEach((choice, index) => {
+      formattedQuestion['choice' + (index + 1)] = choice;
+    });
+    
+    return formattedQuestion;
+  })
+  game.classList.remove('hidden');
+  loader.classList.add('hidden');
   startGame();
 }).catch(err => {
   console.error(err);
